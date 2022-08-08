91e6156fbec775dda74c8df0d2f434dc03cdd990
Randell Dawson <randy@randydawson.com>
2022-08-08 11:20:27 -0600

    feat: added post route to upload a file

--- app/index.js
+++ app/index.js
@@ -1,18 +1,27 @@
 import express from 'express';
 import cors from 'cors';
 import dotenv from 'dotenv';
+import multer from 'multer';
 
 dotenv.config();
-
+const upload = multer({ dest: 'uploads/' })
 const app = express();
 
 app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use('/public', express.static(process.cwd() + '/app/public'));
 
 app.get('/', function (req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+  res.sendFile(process.cwd() + '/app/views/index.html');
 });
 
+app.post('/api/fileanalyse', upload.single('upfile'), (req, res, next) => {
+  const {
+    originalname: name,
+    mimetype: type,
+    size
+  } = req.file;
+  res.json({ name, type, size });
+});
 
 const port = process.env.PORT || 3000;
 app.listen(port, function () {
